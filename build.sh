e2f7b8d356ab301ee864b735af3254c0bf28fefd
Nick Saw <chitsaw@micrsoft.com>
2020-08-28 16:24:03 -0700

    Added build.sh files

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 (cd ./Samples/AzureKinectSample/                                                       && . ./build.sh)
 (cd ./Samples/RosTurtleSample/                                                         && . ./build.sh)
 (cd ./Samples/LinuxSpeechSample/                                                       && . ./build.sh)
+(cd ./Samples/LinuxWebcamWithAudioSample/                                                       && . ./build.sh)
