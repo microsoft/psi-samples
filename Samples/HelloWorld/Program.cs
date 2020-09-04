c5faeaf50c5f30654194fa51eae3dec5b24e24ba
sandrist <sandrist@microsoft.com>
2020-09-03 18:43:03 -0700

    created simply hello world sample and VAD webinar sample

---
+++ Samples/HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace HelloWorld
+{
+    using System;
+    using Microsoft.Psi;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a pipeline
+            var p = Pipeline.Create();
+
+            // Create a timer component that produces a message every second
+            var timer = Timers.Timer(p, TimeSpan.FromSeconds(1));
+
+            // For each message created by the timer, print "Hello world!" and keep track of the count
+            int count = 0;
+            timer.Do(t =>
+            {
+                Console.WriteLine($"{count++}: Hello world!");
+            });
+
+            // Run the pipeline, but don't block here
+            p.RunAsync();
+
+            // Wait for the user to hit a key before closing the pipeline
+            Console.ReadKey();
+
+            // Close the pipeline
+            p.Dispose();
+        }
+    }
+}
---
+++ Samples/SimpleVoiceActivityDetector/Program.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+
+namespace SimpleVoiceActivityDetector
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.Psi;
+    using Microsoft.Psi.Audio;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Pipeline.Create();
+            var store = PsiStore.Create(p, "SimpleVAD", Path.Combine(Directory.GetCurrentDirectory(), "Stores"));
+
+            // Create the microphone, acoustic feature extractor, and connect them
+            var microphone = new AudioCapture(p, WaveFormat.Create16kHz1Channel16BitPcm());
+            var acousticFeaturesExtractor = new AcousticFeaturesExtractor(p);
+            microphone.PipeTo(acousticFeaturesExtractor);
+
+            // Display the log energy
+            acousticFeaturesExtractor.LogEnergy
+                .Sample(TimeSpan.FromSeconds(0.2))
+                .Do(logEnergy => Console.WriteLine($"LogEnergy = {logEnergy}"));
+
+            // Create a voice-activity stream by thresholding the log energy
+            var vad = acousticFeaturesExtractor.LogEnergy
+                .Select(l => l > 7)
+                .Write("VAD", store);
+
+            // Create filtered signal by aggregating over historical buffers
+            var vadWithHistory = acousticFeaturesExtractor.LogEnergy
+                .Window(RelativeTimeInterval.Future(TimeSpan.FromMilliseconds(300)))
+                .Aggregate(false, (previous, buffer) => (!previous && buffer.All(v => v > 7)) || (previous && !buffer.All(v => v < 7)))
+                .Write("VADFiltered", store);
+
+            // Write the microphone output and some acoustic features to the store
+            microphone.Write("Audio", store);
+            acousticFeaturesExtractor.LogEnergy.Write("LogEnergy", store);
+            acousticFeaturesExtractor.ZeroCrossingRate.Write("ZeroCrossingRate", store);
+
+            p.RunAsync();
+            Console.ReadKey();
+            p.Dispose();
+        }
+    }
+}
