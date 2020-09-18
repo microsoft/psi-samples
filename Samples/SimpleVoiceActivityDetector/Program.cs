ce7a9d78d8ed38d1f6c177363e4109a01bf394c8
sandrist <sandrist@microsoft.com>
2020-09-17 20:32:59 -0700

    Simple VAD walkthrough in progress

--- Samples/SimpleVoiceActivityDetector/Program.cs
+++ Samples/SimpleVoiceActivityDetector/Program.cs
@@ -21,10 +21,13 @@ namespace SimpleVoiceActivityDetector
         {
             using (var p = Pipeline.Create())
             {
-                var store = PsiStore.Create(p, "SimpleVAD", Path.Combine(Directory.GetCurrentDirectory(), "Stores"));
-
                 // Create the microphone, acoustic feature extractor, and connect them
                 var microphone = new AudioCapture(p, WaveFormat.Create16kHz1Channel16BitPcm());
+
+                // Comment out the line above, and uncomment the next two lines to run the application by
+                // replaying over existing data, rather than live.
+                ////var inputStore = PsiStore.Open(p, "SimpleVAD", Path.Combine(Directory.GetCurrentDirectory(), "Stores", "SimpleVAD.0000"));
+                ////var microphone = inputStore.OpenStream<AudioBuffer>("Audio");
                 var acousticFeaturesExtractor = new AcousticFeaturesExtractor(p);
                 microphone.PipeTo(acousticFeaturesExtractor);
 
@@ -35,17 +38,18 @@ namespace SimpleVoiceActivityDetector
 
                 // Create a voice-activity stream by thresholding the log energy
                 var vad = acousticFeaturesExtractor.LogEnergy
-                    .Select(l => l > 7)
-                    .Write("VAD", store);
+                    .Select(l => l > 7);
 
                 // Create filtered signal by aggregating over historical buffers
                 var vadWithHistory = acousticFeaturesExtractor.LogEnergy
                     .Window(RelativeTimeInterval.Future(TimeSpan.FromMilliseconds(300)))
-                    .Aggregate(false, (previous, buffer) => (!previous && buffer.All(v => v > 7)) || (previous && !buffer.All(v => v < 7)))
-                    .Write("VADFiltered", store);
+                    .Aggregate(false, (previous, buffer) => (!previous && buffer.All(v => v > 7)) || (previous && !buffer.All(v => v < 7)));
 
-                // Write the microphone output and some acoustic features to the store
+                // Write the microphone output, VAD streams, and some acoustic features to the store
+                var store = PsiStore.Create(p, "SimpleVAD", Path.Combine(Directory.GetCurrentDirectory(), "Stores"));
                 microphone.Write("Audio", store);
+                vad.Write("VAD", store);
+                vadWithHistory.Write("VADFiltered", store);
                 acousticFeaturesExtractor.LogEnergy.Write("LogEnergy", store);
                 acousticFeaturesExtractor.ZeroCrossingRate.Write("ZeroCrossingRate", store);
 
