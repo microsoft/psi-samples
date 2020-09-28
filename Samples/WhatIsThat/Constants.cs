9e1314bcc44aa27e7352f641de410e24d882f3c7
danbohus <dbohus@microsoft.com>
2020-09-28 13:21:34 -0700

    Adding WhatIsThat sample (#7)
    Added WhatIsThat sample project and corresponding walkthrough.

---
+++ Samples/WhatIsThat/Constants.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace WhatIsThat
+{
+    /// <summary>
+    /// Static class containing constants.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        /// This sample depends on having a subscription to the Azure Speech Api. A valid subscription
+        /// key may be obtained by signing up at https://azure.microsoft.com/en-us/try/cognitive-services/.
+        /// </summary>
+        internal static readonly string AzureSpeechSubscriptionKey = string.Empty;
+
+        /// <summary>
+        /// This sample depends on having a subscription to the Azure Computer Vision API. A valid subscription
+        /// key may be obtained by signing up at https://azure.microsoft.com/en-us/try/cognitive-services/.
+        /// </summary>
+        internal static readonly string AzureVisionSubscriptionKey = string.Empty;
+
+        /// <summary>
+        /// This is the Azure region, fill it in according to the region to which the subscription is
+        /// associated (e.g. "westus").
+        /// </summary>
+        internal static readonly string AzureRegion = string.Empty;
+    }
+}
